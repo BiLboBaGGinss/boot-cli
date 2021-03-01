c326154b48f623498eb49b52c635e30ad8f5508e
Pan <geuwgu@gmail.com>
2021-03-01 14:41:35 +0800

    添加自定义用户认证逻辑

--- src/main/java/com/pan/bootcli/config/SecurityConfig.java
+++ src/main/java/com/pan/bootcli/config/SecurityConfig.java
@@ -3,11 +3,15 @@ package com.pan.bootcli.config;
 import com.pan.bootcli.config.security.AuthenticationEntryPointImpl;
 import com.pan.bootcli.config.security.filter.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -16,6 +20,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 自定义用户认证逻辑
+     */
+    @Autowired
+    private UserDetailsService userDetailsService;
     /**
      * 认证失败处理类
      */
@@ -74,5 +83,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // 添加CORS filter
 //        httpSecurity.addFilterBefore(corsFilter, JwtAuthenticationTokenFilter.class);
 //        httpSecurity.addFilterBefore(corsFilter, LogoutFilter.class);
+    }
+
+    /**
+     * 强散列哈希加密实现
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 身份认证接口
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 }
